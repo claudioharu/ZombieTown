fe72aba65511986ecb08db6c28a80a5e5e738689
claudioharu <claudioharu@gmail.com>
2015-05-05 10:37:59 -0300

    Shot spawn

---
+++ rts/Assets/ZombieShoot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieShoot : MonoBehaviour {
+	
+	private GameObject targetEnemy;
+	
+	public GameObject bullet;
+	public LayerMask mask;
+	public Transform bulletSpaw;
+	
+	public float fireDelay = 1.0f;
+	public float radius = 1.0f;
+	public float speed = 100.0f;
+	
+	void Start(){
+		Invoke("Shoot", fireDelay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Collider[] enemysArray = Physics.OverlapSphere(transform.position, radius, mask);
+		
+		if(enemysArray.Length > 0)
+			targetEnemy = enemysArray[0].gameObject;
+		else
+			targetEnemy = null;
+	}
+	
+	void Shoot(){
+		
+		if(targetEnemy != null){
+			GameObject instance = Instantiate(bullet, bulletSpaw.transform.position, bullet.transform.rotation) as GameObject;
+			
+			if(instance.rigidbody){
+				instance.rigidbody.AddForce((targetEnemy.transform.position - transform.position).normalized*speed*Time.deltaTime, ForceMode.VelocityChange);
+			}
+		}
+		
+		Invoke("Shoot", fireDelay);
+	}
+	
+}
