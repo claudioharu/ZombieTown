ff931448eb96b4b89714e6fd48c2d2fbef464484
claudioharu <claudioharu@gmail.com>
2015-05-29 17:39:24 -0300

    zumbi animado
    Novo zumbi

---
+++ rts/Assets/ZombieCharacterPack/ZombieWalk.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieWalk : MonoBehaviour {
+	protected Animator animacao;
+	// Use this for initialization
+	void Start () {
+		animacao = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animacao.SetFloat("veloc", 3*Input.GetAxis("Vertical"));
+	}
+}
