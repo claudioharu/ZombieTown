9362b8df4efac75ee82c9fa315d104a2fb977f27
claudioharu <claudioharu@gmail.com>
2015-05-30 14:00:31 -0300

    Movimentando a camera
    Agora tem zoom e translação. Falta a rotação

---
+++ rts/Assets/scripts/MovingCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCamera : MonoBehaviour {
+
+	public float camSpeed = 1.0f;
+	public float GUIsize = 40.0f;
+
+	
+	// Update is called once per frame
+	void Update () {
+		Rect recDown = new Rect (0,0, Screen.width, GUIsize);
+		Rect recUp = new Rect (0, Screen.height-GUIsize, Screen.width, GUIsize);
+		Rect recLeft = new Rect (0,0, GUIsize, Screen.height);
+		Rect recRight = new Rect (Screen.width-GUIsize, 0, GUIsize, Screen.height);
+
+		if(recDown.Contains (Input.mousePosition))
+			transform.Translate(0,0, -camSpeed, Space.World);
+
+		if(recUp.Contains (Input.mousePosition))
+			transform.Translate(0,0, camSpeed, Space.World);
+
+		if(recLeft.Contains (Input.mousePosition))
+			transform.Translate(-camSpeed, 0,0, Space.World);
+
+		if(recRight.Contains (Input.mousePosition))
+			transform.Translate(camSpeed,0, 0, Space.World);
+
+
+
+	}
+}
---
+++ rts/Assets/scripts/ZoomingCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomingCamera : MonoBehaviour {
+
+
+	//zoom camera
+	public float distance = 60.0f;
+	public float sensitivityDistance = 50.0f;
+	public float damping  = 5.0f;
+	public float minFOV = 20.0f;
+	public float maxFOV = 100.0f;
+	
+
+	// Use this for initialization
+	void Start () {
+		distance = GetComponent<Camera>().fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityDistance;
+		distance = Mathf.Clamp(distance, minFOV, maxFOV);
+		GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, distance, Time.deltaTime * damping);
+	}
+}
