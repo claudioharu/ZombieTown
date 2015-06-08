448bc8a2186aebf59982ac292c1dbbaec6816d7b
claudioharu <claudioharu@gmail.com>
2015-06-08 16:33:38 -0300

    Troquei a camera de perspectiva para ortogonal
    Evita distorções

--- rts/Assets/scripts/ZoomingCamera.cs
+++ rts/Assets/scripts/ZoomingCamera.cs
@@ -5,22 +5,27 @@ public class ZoomingCamera : MonoBehaviour {
 
 
 	//zoom camera
-	public float distance = 60.0f;
-	public float sensitivityDistance = 50.0f;
+	public float distance = 10.0f;
+	public float sensitivityDistance = 45.0f;
 	public float damping  = 5.0f;
-	public float minFOV = 20.0f;
-	public float maxFOV = 100.0f;
+
 	
 
 	// Use this for initialization
 	void Start () {
-		distance = GetComponent<Camera>().fieldOfView;
+		distance = GetComponent<Camera>().orthographicSize;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		distance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityDistance;
-		distance = Mathf.Clamp(distance, minFOV, maxFOV);
-		GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, distance, Time.deltaTime * damping);
+		if (distance <= 45.0f && distance >= 10.0f){
+			distance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityDistance;
+			if(distance >= 45.0f) distance = 45.0f;
+			if(distance <= 10.0f) distance = 10.0f;
+			print(distance);
+
+			GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, distance, Time.deltaTime * damping);
+
+		}
 	}
 }
