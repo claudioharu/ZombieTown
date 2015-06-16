046b3da7a8e07ba27e4cd9b74b242f189a253bf4
claudioharu <claudioharu@gmail.com>
2015-06-16 11:57:12 -0300

    menu

---
+++ rts/Assets/scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void Play(){
+		Application.LoadLevel(1);
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+}
