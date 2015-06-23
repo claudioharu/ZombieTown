475a7455d58968afe0dc8be3aff4584966d63b9a
tieikoff <diegostarlingf@gmail.com>
2015-06-23 11:25:26 -0300

    Menu da Historia consertado

---
+++ rts/Assets/scripts/StoryTextMove.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryTextMove : MonoBehaviour {
+	
+	void FixedUpdate () {
+		transform.position += transform.up * Time.deltaTime*25;
+	}
+}
