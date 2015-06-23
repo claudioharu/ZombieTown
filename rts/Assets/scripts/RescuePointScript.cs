43c25cbc5bc8c4de8cff4e4ec185962c5a7a8ed5
claudioharu <claudioharu@gmail.com>
2015-06-23 19:31:34 -0300

    rescuePoint
    Scripts

---
+++ rts/Assets/scripts/RescuePointScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RescuePointScript : MonoBehaviour {
+
+	public string[] suviversTags;
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		//print ("passei");
+		foreach (string tag in suviversTags)
+		{
+			//atualizar o contador de sobreviventes
+			if (collision.collider.tag == tag)
+			{
+				//print ("destrui");
+				//ScreenSystem.zombiesKilled += 1;
+				Destroy(collision.gameObject);
+				//Destroy(gameObject);
+				return;
+			}
+		}
+	}
+}
