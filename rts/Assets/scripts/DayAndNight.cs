19a0b38677acb4e7d61ce39ae40438b90479b4b7
claudioharu <claudioharu@gmail.com>
2015-06-24 10:18:15 -0300

    dia e noite

---
+++ rts/Assets/scripts/DayAndNight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayAndNight : MonoBehaviour {
+
+	public Light[] lights;
+	public string[] enemiesTag;
+
+	private bool night = false;
+
+	void makingLight(){
+		foreach (Light light in lights){
+			night = !night;
+			light.enabled = !light.enabled;
+		}
+		//Alterar a velocidade dos zumbis
+		Invoke ("makingLight", 2);
+	}
+
+	// Use this for initialization
+	void Start () {
+		Invoke("makingLight", 2);
+	}
+	
+
+}
