edf715eda7faf873b09083aabc340bec793602d5
claudioharu <claudioharu@gmail.com>
2015-06-26 14:50:22 -0300

    terminei de colocar as casas no navmesh. Adicionei um frame ao mapa
    Coloquei um frame no mapa, mas ele ainda esta invisível. terminei de
    colocar as casas no navmesh

---
+++ rts/Assets/scripts/ZombieSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSpawn : MonoBehaviour {
+
+	public Transform spawnPos;
+	public GameObject zombie;
+	public float birthRate = 10f;
+
+	void Start () {
+		Invoke("ZombiesBirth", birthRate);
+	}
+
+	void ZombiesBirth(){
+		Instantiate(zombie, spawnPos.position, spawnPos.rotation);
+		Invoke("ZombiesBirth", birthRate);
+	}
+}
